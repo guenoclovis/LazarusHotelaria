81077c78038f50065c3e3eb77ac257dfe7ddd377
Vanessa <Vanessa@Wosgrau-PC>
2017-09-17 22:18:29 -0300

    ajustes merge eduardo

--- src/main/webapp/app/app.modulo-principal.js
+++ src/main/webapp/app/app.modulo-principal.js
@@ -2,10 +2,6 @@
 	'use strict';
 
 	var myApp = angular.module('lazarusApp', [ 'ui.router', 'restangular',
-<<<<<<< HEAD
-			'cliente', 'filial', 'atributo' ]);
-=======
 			'cliente', 'filial', 'atributo', 'showcase' ]);
->>>>>>> branch 'master' of https://github.com/guenoclovis/LazarusHotelaria.git
 
 })();
--- src/main/webapp/app/atributo/atributo-consultar.controller.js
+++ src/main/webapp/app/atributo/atributo-consultar.controller.js
@@ -67,17 +67,10 @@
             $state.reload();
         }
 
-<<<<<<< HEAD
         function irParaTelaDetalhamento(CodAtributo) {
             //salvarEstadoTela();			
             $state.go('atributoDetalhar', {
                 'codAtributo': CodAtributo
-=======
-        function irParaTelaDetalhamento(id) {
-            //salvarEstadoTela();			
-            $state.go('atributoDetalhar', {
-                'codAtributo': id
->>>>>>> branch 'master' of https://github.com/guenoclovis/LazarusHotelaria.git
             });
         }
 
