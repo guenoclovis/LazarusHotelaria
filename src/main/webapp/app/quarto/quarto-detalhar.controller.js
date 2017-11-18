42f969301defadd52075a74b6c337c53b345e449
clovisgueno <clovis.gueno@gmail.com>
2017-11-18 12:26:17 -0200

    CRUD de Quarto

---
+++ src/main/webapp/app/quarto/quarto-detalhar.controller.js
@@ -0,0 +1,65 @@
+//-----------------------------------------------------
+// CONTROLADOR DETALHAMENTO
+//-----------------------------------------------------
+(function () {
+    'use strict';
+
+    // Adicionando um Controlador para a tela 'detalhar' do modulo 'quarto'
+    angular.module('quarto').controller(
+            'DetalharQuartoController',
+            DetalharQuartoController);
+
+    // Definindo atributos e operacoes do Controlador da tela 'detalhar' do modulo 'quarto'
+    /* @ngInject */
+    function DetalharQuartoController($controller, $scope, $state, $stateParams,
+    		QuartoData) {
+
+        //////// ATRIBUTOS DO CONTROLADOR ////////////////////
+        var vm = this;
+
+        vm.quarto = {};
+        vm.quarto.codQuarto = $stateParams.codQuarto;
+
+        vm.msgs = "";
+
+        vm.voltarParaTelaAnterior = voltarParaTelaAnterior;
+        vm.irParaTelaEdicao = irParaTelaEdicao;
+        vm.excluir = excluir;
+
+        activate();
+
+        //////// OPERACOES DO CONTROLADOR ////////////////////
+
+        function activate() {
+            obter();
+        }
+
+        function limpar() {
+            $state.reload();
+        }
+
+        function voltarParaTelaAnterior() {
+            $state.go('quartoConsultar');
+        }
+
+        function irParaTelaEdicao(codQuarto) {
+            $state.go('quartoEditar', {
+                'codQuarto': codQuarto
+            });
+        }
+
+        function obter() {
+        	QuartoData.obter(vm.quarto.codQuarto).then(function (data) {
+                vm.quarto = data.plain();
+            });
+        }
+        
+        function excluir() {
+        	QuartoData.excluir(vm.quarto.codQuarto).then(function (data) {                
+                $state.go('quartoConsultar');
+            });
+        }
+
+    }
+
+})();
