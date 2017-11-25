c1d968b9ba73f4ba83179b3fca8cd7c36e62410f
clovisgueno <clovis.gueno@gmail.com>
2017-11-25 17:57:41 -0200

    correcao de erro apos o merge

--- src/main/webapp/app/login/seguranca.http-interceptor.js
+++ src/main/webapp/app/login/seguranca.http-interceptor.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function() {
 	'use strict';
 
@@ -57,66 +56,4 @@
 	function configure($httpProvider) {
 		$httpProvider.interceptors.push('AuthInterceptor');
 	}
-
-=======
-(function() {
-	'use strict';
-
-	angular.module('login')
-		.service('AuthInterceptor', AuthInterceptor)
-		.config(configure);
-
-
-	 /* @ngInject */
-    function AuthInterceptor($q, $window, $rootScope, $injector, MsgCenter) {
-        return {
-        	
-            request: function(request){
-            	if(/.*obterImgCaptcha.*/.test(request.url) ||
-            	   /.*obterSomcaptcha.*/.test(request.url) ||
-            	   (/.*login.*/.test(request.url) && request.method == 'POST')){
-            		if(request.headers.Authorization){
-            			delete request.headers.Authorization;            			
-            		}
-            	}
-            	
-            	return request;            	
-            },
-            
-        	responseError: function (response) {
-                var status = response.status;
-                
-                if (status === 401) {
-                    var $auth = $injector.get('$auth');
-                    $auth.removeToken();
-                	$window.location.href = $rootScope.baseURL + "/login";
-                }
-                
-                if (status === 403) {
-                    var $state = $injector.get('$state');
-                    MsgCenter.addNext(response.data.severity, response.data.message);
-                    $state.go('erropermissao');                	
-                }
-                
-                return $q.reject(response);
-            },
-            
-            response: function (response) { 
-            	if(/.*rest-clovis.*/.test(response.config.url)){
-            		
-            		var segurancaTokenService = $injector.get('SegurancaTokenService');            		
-            		segurancaTokenService.start();            		
-            	}
-            	
-                return response;
-            }
-        };
-    }
-
-	/* @ngInject */
-	function configure($httpProvider) {
-		$httpProvider.interceptors.push('AuthInterceptor');
-	}
-
->>>>>>> branch 'master' of https://github.com/guenoclovis/LazarusHotelaria.git
 })();
